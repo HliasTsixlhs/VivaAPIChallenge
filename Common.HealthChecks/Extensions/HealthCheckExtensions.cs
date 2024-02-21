a1e97e89a96128d9c657439b98381665c2bcd45d
itsichlis <itsichlis@indeavor.com>
2024-02-21 15:29:31 +0200

    Add Common.Healthchecks lib project

---
+++ Common.HealthChecks/Extensions/HealthCheckExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Common.HealthChecks.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using HealthChecks.UI.Client;
+using Microsoft.Extensions.Configuration;
+
+namespace Common.HealthChecks.Extensions
+{
+    public static class HealthCheckExtensions
+    {
+        public static IServiceCollection AddCustomHealthChecks(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var healthCheckUIOptions = new HealthCheckUIOptions();
+            configuration.Bind("HealthCheckUI", healthCheckUIOptions);
+
+            services.AddHealthChecks()
+                .AddCheck("liveness", () => HealthCheckResult.Healthy());
+
+            services.AddHealthChecksUI(setupSettings =>
+                    {
+                        setupSettings.AddHealthCheckEndpoint("Service's self Health.", "health");
+                        setupSettings.SetEvaluationTimeInSeconds(healthCheckUIOptions.EvaluationTimeInSeconds);
+                        setupSettings.MaximumHistoryEntriesPerEndpoint(healthCheckUIOptions.MaximumHistoryEntriesPerEndpoint);
+                        setupSettings.SetApiMaxActiveRequests(healthCheckUIOptions.ApiMaxActiveRequests);
+                        setupSettings.SetMinimumSecondsBetweenFailureNotifications(healthCheckUIOptions.MinimumSecondsBetweenFailureNotifications);
+                    }
+                )
+                .AddInMemoryStorage();
+            return services;
+        }
+
+        public static IApplicationBuilder UseCustomHealthChecks(this IApplicationBuilder app)
+        {
+            app.UseHealthChecks("/self", new HealthCheckOptions
+            {
+                Predicate = r => r.Name.Contains("self")
+            });
+
+            app.UseHealthChecks("/health", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecksUI(options =>
+            {
+                options.UIPath = "/health-ui";
+                options.ApiPath = "/health-ui-api";
+            });
+
+            return app;
+        }
+    }
+}
---
+++ Common.HealthChecks/Options/HealthCheckUIOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace Common.HealthChecks.Options;
+
+public class HealthCheckUIOptions
+{
+    // Defines the interval in seconds for evaluating health checks.
+    // The UI will poll health checks based on this time.
+    public int EvaluationTimeInSeconds { get; set; }
+
+    // Sets the maximum number of execution history records per endpoint.
+    // Helps in maintaining the size of the stored history.
+    public int MaximumHistoryEntriesPerEndpoint { get; set; }
+
+    // Limits the maximum number of concurrent requests to the health check API.
+    // Helps in controlling the load on the health check endpoint.
+    public int ApiMaxActiveRequests { get; set; }
+
+    // Determines the minimum duration in seconds between consecutive notifications for a failure.
+    // Useful to prevent too frequent notifications in case of failing health checks.
+    public int MinimumSecondsBetweenFailureNotifications { get; set; }
+}
