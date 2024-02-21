8aac3fcf4c569d21ca58d51624255200e5f6060b
itsichlis <itsichlis@indeavor.com>
2024-02-21 17:55:56 +0200

    Add ISecondLargestNumberService and SecondLargestNumberService

---
+++ Viva.Geo.API.Core/Abstractions/Services/ISecondLargestNumberService.cs
@@ -0,0 +1,14 @@
+﻿namespace Viva.Geo.API.Core.Abstractions.Services;
+
+/// <summary>
+/// Interface for a service that provides functionality to find the second largest number in an array.
+/// </summary>
+public interface ISecondLargestNumberService
+{
+    /// <summary>
+    /// Finds the second largest number in the provided array of integers.
+    /// </summary>
+    /// <param name="numbers">The array of integers to process.</param>
+    /// <returns>The second largest integer in the array.</returns>
+    int FindSecondLargest(IEnumerable<int> numbers);
+}
---
+++ Viva.Geo.API.Core/Services/SecondLargestNumberService.cs
@@ -0,0 +1,23 @@
+﻿using Viva.Geo.API.Core.Abstractions.Services;
+
+namespace Viva.Geo.API.Core.Services;
+
+/// <summary>
+/// Service for processing arrays and finding the second largest number.
+/// </summary>
+public class SecondLargestNumberService : ISecondLargestNumberService
+{
+    /// <summary>
+    /// Finds the second largest number in an array using LINQ.
+    /// </summary>
+    /// <param name="numbers">The array of integers to process.</param>
+    /// <returns>The second largest integer in the array.</returns>
+    public int FindSecondLargest(IEnumerable<int> numbers)
+    {
+        return numbers
+            .OrderByDescending(n => n)
+            .Distinct()
+            .Skip(1)
+            .FirstOrDefault();
+    }
+}
