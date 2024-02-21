b926c4a2ef052a62f74796e9581ccb3ed09ae2c2
itsichlis <itsichlis@indeavor.com>
2024-02-21 17:56:26 +0200

    Add ArrayProcessingController

---
+++ Viva.Geo.API/Controllers/ArrayProcessingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Viva.Geo.API.Common.Dtos.ArrayProcessing.Requests;
+using Viva.Geo.API.Core.Abstractions.Services;
+
+
+namespace Viva.Geo.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ArrayProcessingController : ControllerBase
+{
+    private readonly ISecondLargestNumberService _secondLargestNumberService;
+
+    public ArrayProcessingController(ISecondLargestNumberService secondLargestNumberService)
+    {
+        _secondLargestNumberService = secondLargestNumberService;
+    }
+
+    [HttpPost("FindSecondLargest")]
+    public IActionResult FindSecondLargest([FromBody] SecondLargestNumberRequest request)
+    {
+        var secondLargest = _secondLargestNumberService.FindSecondLargest(request.Numbers);
+        return Ok(secondLargest);
+    }
+}
