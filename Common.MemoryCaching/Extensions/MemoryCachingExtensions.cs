8905e5872619c6b68b02554052d88e34e03af395
itsichlis <itsichlis@indeavor.com>
2024-02-22 13:02:20 +0200

    Remove custom MemoryOptions and use Microsoft's instead
    - also update csproj to include cashing lib

--- Common.MemoryCaching/Extensions/MemoryCachingExtensions.cs
+++ Common.MemoryCaching/Extensions/MemoryCachingExtensions.cs
@@ -1,15 +1,43 @@
-﻿// MemoryCachingExtensions.cs
-
-using Common.MemoryCaching.Abstractions;
+﻿using Common.MemoryCaching.Abstractions;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Common.MemoryCaching.Extensions;
 
 public static class MemoryCachingExtensions
 {
-    public static IServiceCollection AddMemoryCachingServices(this IServiceCollection services)
+    /// <summary>
+    /// Adds memory caching services to the specified IServiceCollection.
+    /// </summary>
+    /// <param name="services">The IServiceCollection to add services to.</param>
+    /// <param name="configuration">The application's configuration.</param>
+    /// <returns>The IServiceCollection.</returns>
+    public static IServiceCollection AddMemoryCachingServices(this IServiceCollection services,
+        IConfiguration configuration)
     {
-        services.AddMemoryCache();
+        var memoryCacheOptions = new MemoryCacheOptions();
+        configuration.Bind("MemoryCacheOptions", memoryCacheOptions);
+
+        // Configure the memory cache with options from configuration
+        services.AddMemoryCache(options =>
+        {
+            // Set the size limit of the memory cache
+            options.SizeLimit = memoryCacheOptions.SizeLimit;
+
+            // Set the frequency to scan for expired items
+            options.ExpirationScanFrequency = memoryCacheOptions.ExpirationScanFrequency;
+
+            // Set the percentage of cache to compact when the size limit is exceeded
+            options.CompactionPercentage = memoryCacheOptions.CompactionPercentage;
+
+            // Determines whether to track linked cache entries (used in scenarios like child actions in MVC)
+            options.TrackLinkedCacheEntries = memoryCacheOptions.TrackLinkedCacheEntries;
+
+            // Enables tracking of detailed memory cache statistics
+            options.TrackStatistics = memoryCacheOptions.TrackStatistics;
+        });
+
         services.AddSingleton<IMemoryCacheService, MemoryCacheService>();
         return services;
     }
