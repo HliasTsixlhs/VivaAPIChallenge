9ebbc7b274211f908a797ec0264fa48b1c70e383
itsichlis <itsichlis@indeavor.com>
2024-02-22 11:48:28 +0200

    Add BorderRepository and Interface

---
+++ Viva.Geo.API.Core/Abstractions/Repositories/IBorderRepository.cs
@@ -0,0 +1,9 @@
+﻿using Common.Persistence.EFCore.Abstractions;
+using Viva.Geo.API.DataAccess.DataAccessModels;
+
+namespace Viva.Geo.API.Core.Abstractions.Repositories;
+
+public interface IBorderRepository : IEfCoreRepository<Border, int>
+{
+    // Task<Border> CreateBorderAsync(Border border, CancellationToken cancellationToken = default);
+}
---
+++ Viva.Geo.API.Core/Repositories/BorderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Common.Persistence.EFCore;
+using Viva.Geo.API.Core.Abstractions.Repositories;
+using Viva.Geo.API.DataAccess.Context;
+using Viva.Geo.API.DataAccess.DataAccessModels;
+
+namespace Viva.Geo.API.Core.Repositories;
+
+public class BorderRepository : BaseRepository<Border, int, GeoContext>, IBorderRepository
+{
+    public BorderRepository(GeoContext context) : base(context)
+    {
+    }
+}
