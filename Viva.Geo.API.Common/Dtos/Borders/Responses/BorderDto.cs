7e2febc80cad9a932f6e9ec24f0ce07b274c5777
itsichlis <itsichlis@indeavor.com>
2024-02-22 12:04:03 +0200

    Add Dtos models

---
+++ Viva.Geo.API.Common/Dtos/Borders/Responses/BorderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Viva.Geo.API.Common.Dtos.Borders.Responses;
+
+public class BorderDto
+{
+    public int BorderId { get; set; }
+    public string BorderCode { get; set; } // For example, "ALB", "BGR", etc.
+
+    // Navigation property for the many-to-many relationship
+    // public ICollection<CountryBorder> CountryBorders { get; set; }
+}
---
+++ Viva.Geo.API.Common/Dtos/Countries/Responses/CountryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Viva.Geo.API.Common.Dtos.Countries.Responses;
+
+public class CountryDto
+{
+    public string CommonName { get; set; }
+    public string Capital { get; set; }
+    public List<string> Borders { get; set; }
+}
