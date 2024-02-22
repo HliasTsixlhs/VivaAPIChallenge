3ae04b47f2043e1302ac459d8569abfbbc3cfd72
itsichlis <itsichlis@indeavor.com>
2024-02-22 11:44:29 +0200

    Add CountriesController

---
+++ Viva.Geo.API/Controllers/CountriesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Viva.Geo.API.Core.Abstractions.Services;
+
+namespace Viva.Geo.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CountriesController : ControllerBase
+{
+    private readonly ICountryService _countryService;
+
+    public CountriesController(ICountryService countryService)
+    {
+        _countryService = countryService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> RetrieveAndSaveCountries()
+    {
+        var countries = await _countryService.RetrieveAndSaveCountriesAsync();
+        if (countries == null || !countries.Any())
+        {
+            return NotFound("No countries data found.");
+        }
+
+        return Ok(countries);
+    }
+
+    [HttpGet("{countryName}")]
+    public async Task<IActionResult> RetrieveAndSaveCountry(string countryName)
+    {
+        var countryDto = await _countryService.RetrieveAndSaveCountryByNameAsync(countryName);
+        if (countryDto == null)
+        {
+            return NotFound($"Country data for {countryName} not found.");
+        }
+
+        return Ok(countryDto);
+    }
+}
