0c2bf7ebd09946e814f2cbf0904c3de736811212
itsichlis <itsichlis@indeavor.com>
2024-02-22 11:46:03 +0200

    Add CountryRepository and Interface

---
+++ Viva.Geo.API.Core/Abstractions/Repositories/ICountryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Common.Persistence.EFCore.Abstractions;
+using Viva.Geo.API.DataAccess.DataAccessModels;
+
+namespace Viva.Geo.API.Core.Abstractions.Repositories;
+
+public interface ICountryRepository : IEfCoreRepository<Country, int>
+{
+    Task<Country> GetCountryByNameAsync(string name, CancellationToken cancellationToken = default);
+    Task<Country> CreateCountryAsync(Country country, CancellationToken cancellationToken = default);
+}
---
+++ Viva.Geo.API.Core/Repositories/CountryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Common.Persistence.EFCore;
+using Viva.Geo.API.Core.Abstractions.Repositories;
+using Viva.Geo.API.DataAccess.Context;
+using Viva.Geo.API.DataAccess.DataAccessModels;
+
+namespace Viva.Geo.API.Core.Repositories;
+
+public class CountryRepository : BaseRepository<Country, int, GeoContext>, ICountryRepository
+{
+    public CountryRepository(GeoContext context) : base(context)
+    {
+    }
+
+    public async Task<Country> GetCountryByNameAsync(string name, CancellationToken cancellationToken = default)
+    {
+        // Use SearchAsync to find a country by its name
+        var countries = await SearchAsync(c => c.CommonName == name, cancellationToken);
+        return countries.FirstOrDefault();
+    }
+
+    public async Task<Country> CreateCountryAsync(Country country, CancellationToken cancellationToken = default)
+    {
+        var createdCountry = await CreateAsync(country, cancellationToken);
+        return createdCountry;
+    }
+}
