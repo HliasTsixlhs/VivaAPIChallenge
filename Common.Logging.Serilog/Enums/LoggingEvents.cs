1382086b6b25ac12b780c62a794bba6372f1d7d8
itsichlis <itsichlis@indeavor.com>
2024-02-22 17:03:51 +0200

    Update loggingEvents and csproj

--- Common.Logging.Serilog/Enums/LoggingEvents.cs
+++ Common.Logging.Serilog/Enums/LoggingEvents.cs
@@ -1,26 +1,9 @@
 ﻿namespace Common.Logging.Serilog.Enums;
 
 // Note: Please ensure these settings are updated in accordance with the evolving business logic of the application.
-public enum BankServiceEvent
+public enum VivaGeoApiEvent
 {
-    TransactionProcessed = 1000,
-    AccountCreated,
-    AccountClosed,
-    // Add other event types as needed
-}
-
-public enum FinancialAnalyticsServiceEvent
-{
-    DataAnalysisStarted = 2000,
-    ReportGenerated,
-    DataImported,
-    // Add other event types as needed
-}
-
-public enum BffWebBankMqApiEvent
-{
-    MessageReceived = 3000,
-    RequestForwarded,
-    ResponseReceived,
-    // Add other event types as needed
+    CountryProcessing = 1000, // Event for country data retrieval
+    BorderProcessing,        // Event for processing border data
+    ArrayProcessing          // Event for array processing tasks
 }
