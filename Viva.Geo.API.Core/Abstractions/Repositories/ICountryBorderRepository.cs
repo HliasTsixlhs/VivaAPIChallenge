f173af070eea838490f746ab4be0f067de8a5cb6
itsichlis <itsichlis@indeavor.com>
2024-02-22 11:46:30 +0200

    Add CountryBorderRepository and Interface

---
+++ Viva.Geo.API.Core/Abstractions/Repositories/ICountryBorderRepository.cs
@@ -0,0 +1,9 @@
+﻿using Viva.Geo.API.DataAccess.DataAccessModels;
+using Common.Persistence.EFCore.Abstractions;
+
+namespace Viva.Geo.API.Core.Abstractions.Repositories;
+
+public interface ICountryBorderRepository : IEfCoreRepository<CountryBorder, (int CountryId, int BorderId)>
+{
+    Task<CountryBorder> GetAsync(int countryId, int borderId, CancellationToken cancellationToken = default);
+}
---
+++ Viva.Geo.API.Core/Repositories/CountryBorderRepository.cs
@@ -0,0 +1,21 @@
+﻿using Viva.Geo.API.DataAccess.Context;
+using Viva.Geo.API.Core.Abstractions.Repositories;
+using Viva.Geo.API.DataAccess.DataAccessModels;
+using Common.Persistence.EFCore;
+
+namespace Viva.Geo.API.Core.Repositories;
+
+public class CountryBorderRepository : BaseRepository<CountryBorder, (int CountryId, int BorderId), GeoContext>,
+    ICountryBorderRepository
+{
+    public CountryBorderRepository(GeoContext context) : base(context)
+    {
+    }
+
+    public async Task<CountryBorder> GetAsync(int countryId, int borderId,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await SearchAsync(cb => cb.CountryId == countryId && cb.BorderId == borderId, cancellationToken);
+        return result.FirstOrDefault();
+    }
+}
