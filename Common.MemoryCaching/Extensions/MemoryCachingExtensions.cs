1625b5c4fad410422c65f5c48eecd14d068b1f5f
itsichlis <itsichlis@indeavor.com>
2024-02-22 09:50:21 +0200

    Add MemorytChashing extensions

---
+++ Common.MemoryCaching/Extensions/MemoryCachingExtensions.cs
@@ -0,0 +1,16 @@
+﻿// MemoryCachingExtensions.cs
+
+using Common.MemoryCaching.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.MemoryCaching.Extensions;
+
+public static class MemoryCachingExtensions
+{
+    public static IServiceCollection AddMemoryCachingServices(this IServiceCollection services)
+    {
+        services.AddMemoryCache();
+        services.AddSingleton<IMemoryCacheService, MemoryCacheService>();
+        return services;
+    }
+}
