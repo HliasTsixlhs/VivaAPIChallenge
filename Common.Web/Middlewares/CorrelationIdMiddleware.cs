738b00d7678f52634493c24a3828ae658649910d
itsichlis <itsichlis@indeavor.com>
2024-02-24 09:33:44 +0200

    Add docstings and formats

--- Common.Web/Middlewares/CorrelationIdMiddleware.cs
+++ Common.Web/Middlewares/CorrelationIdMiddleware.cs
@@ -13,10 +13,21 @@ public class CorrelationIdMiddleware
         _next = next;
     }
 
+    /// <summary>
+    /// Invokes the middleware to process the HTTP context.
+    /// </summary>
+    /// <param name="context">The HttpContext for the current request.</param>
+    /// <returns>A Task that completes when the middleware has completed processing.</returns>
+    /// <remarks>
+    /// This middleware extracts or generates a unique Correlation ID for each request.
+    /// The Correlation ID is used to track and correlate logs and activities across
+    /// distributed systems and services, aiding in tracing and monitoring.
+    /// </remarks>
     public async Task InvokeAsync(HttpContext context)
     {
         // Extract or generate a Correlation ID
-        var correlationId = context.Request.Headers[CorrelationIdHeaderName].FirstOrDefault() ?? Guid.NewGuid().ToString();
+        var correlationId = context.Request.Headers[CorrelationIdHeaderName].FirstOrDefault() ??
+                            Guid.NewGuid().ToString();
 
         // Add the Correlation ID to the response headers for client transparency
         context.Response.Headers[CorrelationIdHeaderName] = correlationId;
