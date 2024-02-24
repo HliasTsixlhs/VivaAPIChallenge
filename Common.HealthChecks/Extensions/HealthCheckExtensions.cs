5ed0bee223653b834143efdfb93bde09a49a3b09
itsichlis <itsichlis@indeavor.com>
2024-02-24 11:12:28 +0200

    Simplify namespace at healthchecks and remove redundant parenthesis

--- Common.HealthChecks/Extensions/HealthCheckExtensions.cs
+++ Common.HealthChecks/Extensions/HealthCheckExtensions.cs
@@ -6,52 +6,51 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 using HealthChecks.UI.Client;
 using Microsoft.Extensions.Configuration;
 
-namespace Common.HealthChecks.Extensions
+namespace Common.HealthChecks.Extensions;
+
+public static class HealthCheckExtensions
 {
-    public static class HealthCheckExtensions
+    public static IServiceCollection AddCustomHealthChecks(this IServiceCollection services,
+        IConfiguration configuration)
     {
-        public static IServiceCollection AddCustomHealthChecks(this IServiceCollection services,
-            IConfiguration configuration)
+        var healthCheckUIOptions = new HealthCheckUIOptions();
+        configuration.Bind("HealthCheckUI", healthCheckUIOptions);
+
+        services.AddHealthChecks()
+            .AddCheck("liveness", () => HealthCheckResult.Healthy());
+
+        services.AddHealthChecksUI(setupSettings =>
+                {
+                    setupSettings.AddHealthCheckEndpoint("Service's self Health.", "health");
+                    setupSettings.SetEvaluationTimeInSeconds(healthCheckUIOptions.EvaluationTimeInSeconds);
+                    setupSettings.MaximumHistoryEntriesPerEndpoint(healthCheckUIOptions.MaximumHistoryEntriesPerEndpoint);
+                    setupSettings.SetApiMaxActiveRequests(healthCheckUIOptions.ApiMaxActiveRequests);
+                    setupSettings.SetMinimumSecondsBetweenFailureNotifications(healthCheckUIOptions.MinimumSecondsBetweenFailureNotifications);
+                }
+            )
+            .AddInMemoryStorage();
+        return services;
+    }
+
+    public static IApplicationBuilder UseCustomHealthChecks(this IApplicationBuilder app)
+    {
+        app.UseHealthChecks("/self", new HealthCheckOptions
         {
-            var healthCheckUIOptions = new HealthCheckUIOptions();
-            configuration.Bind("HealthCheckUI", healthCheckUIOptions);
+            Predicate = r => r.Name.Contains("self")
+        });
 
-            services.AddHealthChecks()
-                .AddCheck("liveness", () => HealthCheckResult.Healthy());
-
-            services.AddHealthChecksUI(setupSettings =>
-                    {
-                        setupSettings.AddHealthCheckEndpoint("Service's self Health.", "health");
-                        setupSettings.SetEvaluationTimeInSeconds(healthCheckUIOptions.EvaluationTimeInSeconds);
-                        setupSettings.MaximumHistoryEntriesPerEndpoint(healthCheckUIOptions.MaximumHistoryEntriesPerEndpoint);
-                        setupSettings.SetApiMaxActiveRequests(healthCheckUIOptions.ApiMaxActiveRequests);
-                        setupSettings.SetMinimumSecondsBetweenFailureNotifications(healthCheckUIOptions.MinimumSecondsBetweenFailureNotifications);
-                    }
-                )
-                .AddInMemoryStorage();
-            return services;
-        }
-
-        public static IApplicationBuilder UseCustomHealthChecks(this IApplicationBuilder app)
+        app.UseHealthChecks("/health", new HealthCheckOptions
         {
-            app.UseHealthChecks("/self", new HealthCheckOptions
-            {
-                Predicate = r => r.Name.Contains("self")
-            });
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
 
-            app.UseHealthChecks("/health", new HealthCheckOptions()
-            {
-                Predicate = _ => true,
-                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-            });
+        app.UseHealthChecksUI(options =>
+        {
+            options.UIPath = "/health-ui";
+            options.ApiPath = "/health-ui-api";
+        });
 
-            app.UseHealthChecksUI(options =>
-            {
-                options.UIPath = "/health-ui";
-                options.ApiPath = "/health-ui-api";
-            });
-
-            return app;
-        }
+        return app;
     }
 }
