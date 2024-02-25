945d776afbc6d18541f3687e1919069ea6f7802d
itsichlis <itsichlis@indeavor.com>
2024-02-25 21:56:39 +0200

    Add TestConstants class

---
+++ Viva.Geo.API.IntegrationTests/Constants/TestConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Viva.Geo.API.IntegrationTests.Constants;
+
+public static class TestConstants
+{
+    public const string ArrayProcessingSecondLargestEndpoint = "/api/v1/ArrayProcessing/second-largest";
+    public const string CountriesEndpoint = "/api/v1/Countries";
+}
